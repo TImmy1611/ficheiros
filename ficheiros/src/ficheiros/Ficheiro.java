04c132d4c45b8599e87d170b5d97eb5518512d2e
Timoteo <francis16timoteo@hotmail.com>
2022-10-03 16:02:25 +0100

    ModificarFicheiros

--- ficheiros/src/ficheiros/Ficheiro.java
+++ ficheiros/src/ficheiros/Ficheiro.java
@@ -4,10 +4,10 @@
  */
 package ficheiros;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -21,7 +21,7 @@ class Ficheiro {
         nome = Ficheiros.ler.nextLine();
         nome = Ficheiros.ler.next();
         File ficheiro = new File(nome);
-        if(!ficheiro.exists()){
+        if (!ficheiro.exists()) {
             try {
                 ficheiro.createNewFile();
             } catch (IOException ex) {
@@ -31,28 +31,59 @@ class Ficheiro {
     }
 
     static void verificaFicheiro() {
-        
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar");
+        nome = Ficheiros.ler.nextLine();
+        nome = Ficheiros.ler.next();
+        File ficheiro = new File(nome);
+        if (!ficheiro.exists()) {
+            System.out.println("O ficheiro não existe");
+        } else {
+            System.out.println("O ficheiro existe");
+        }
+
     }
 
     static void escreveFicheiro() {
-        
+        String nome;
+        System.out.println("Insira o nome do ficheiro a alterar");
+        nome = Ficheiros.ler.nextLine();
+        nome = Ficheiros.ler.next();
+        File ficheiro = new File(nome);
+        try {
+            if (!ficheiro.exists()) {
+                ficheiro.createNewFile();
+            }
+            FileWriter fw = new FileWriter(ficheiro, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            String texto;
+            System.out.println("Insira o Conteudo");
+            texto = Ficheiros.ler.nextLine();
+            texto = Ficheiros.ler.nextLine();
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+            fw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     static void escreveNovoFicheiro() {
-        
+
     }
 
     static void leituraFicheiro() {
-        
+
     }
 
     static void mostraFicheiro() {
         File ficheiro = new File("C:\\Users\\ftimoteo\\Desktop\\Cen\\cenca\\java\\ficheiros\\ficheiros");
         File[] lista = ficheiro.listFiles();
         System.out.println(">>>>>>>Lista de Ficheiros: <<<<<<<");
-        for(int x=0;x<lista.length;x++){
+        for (int x = 0; x < lista.length; x++) {
             System.out.println(lista[x].getName());
         }
     }
-    
+
 }
