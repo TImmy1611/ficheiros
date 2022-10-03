121916da49502cfae7e43dccb4820f58f2ef48ca
Timoteo <francis16timoteo@hotmail.com>
2022-10-03 15:23:00 +0100

    CriarFicheiros

--- ficheiros/src/ficheiros/Ficheiro.java
+++ ficheiros/src/ficheiros/Ficheiro.java
@@ -4,10 +4,55 @@
  */
 package ficheiros;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ftimoteo
  */
 class Ficheiro {
+
+    static void criaFicheiro() {
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar");
+        nome = Ficheiros.ler.nextLine();
+        nome = Ficheiros.ler.next();
+        File ficheiro = new File(nome);
+        if(!ficheiro.exists()){
+            try {
+                ficheiro.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    static void verificaFicheiro() {
+        
+    }
+
+    static void escreveFicheiro() {
+        
+    }
+
+    static void escreveNovoFicheiro() {
+        
+    }
+
+    static void leituraFicheiro() {
+        
+    }
+
+    static void mostraFicheiro() {
+        File ficheiro = new File("C:\\Users\\ftimoteo\\Desktop\\Cen\\cenca\\java\\ficheiros\\ficheiros");
+        File[] lista = ficheiro.listFiles();
+        System.out.println(">>>>>>>Lista de Ficheiros: <<<<<<<");
+        for(int x=0;x<lista.length;x++){
+            System.out.println(lista[x].getName());
+        }
+    }
     
 }
--- ficheiros/src/ficheiros/Ficheiros.java
+++ ficheiros/src/ficheiros/Ficheiros.java
@@ -1,20 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ficheiros;
 
-/**
- *
- * @author ftimoteo
- */
-public class Ficheiros {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Ficheiros {
+    public static Scanner ler = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        int op;
+        do{
+            System.out.println("1-Criar Ficheiro");
+            System.out.println("2-Verifica se ficheiro existe");
+            System.out.println("3-Escrever em ficheiro mantendo dados(existentes)");
+            System.out.println("4-Escrever um novo ficheiro");
+            System.out.println("5-Ler um ficheiro");
+            System.out.println("6- Mostrar lista de ficheiros");
+            System.out.println("0-Sair");
+            System.out.println("Selecione a opção pretendida");
+            op=ler.nextInt();
+            switch(op){
+                case 1: Ficheiro.criaFicheiro();break;
+                case 2: Ficheiro.verificaFicheiro();break;
+                case 3: Ficheiro.escreveFicheiro();break;
+                case 4: Ficheiro.escreveNovoFicheiro();break;
+                case 5: Ficheiro.leituraFicheiro();break;
+                case 6: Ficheiro.mostraFicheiro();break;
+                case 0: break;
+            }
+        }while(op!=0);
     }
+
     
 }
